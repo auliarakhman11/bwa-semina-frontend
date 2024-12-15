78667df5e56cae1088acd389cf92afb91181aa3f
auliarakhman11 <auliarakhman234@gmail.com>
2024-12-15 08:50:08 +0800

    test alert login

--- src/pages/singin/index.js
+++ src/pages/singin/index.js
@@ -11,27 +11,48 @@ function PageSignin() {
         password: '',
     });
 
+    const [alert, setAlert] = useState({
+        status: false,
+        message: '',
+        type: '',
+    });
+
     const handleChange = (e)=>{
         setForm({...form, [e.target.name]: e.target.value});
     };
 
     const handleSubmit = async ()=>{
         try {
-            const res = await axios.post('http://localhost:9000/api/v1/cms/auth/signin', {
+            const res = await axios.post('http://localhost:9000/api/v1/cms/auth/signin',
+            // form
+            { 
                 email: form.email,
                 password: form.password
             });
 
-            console.log(res);
+            // console.log(res.data.data.token); 
+
+            setAlert({
+                status: true,
+                message:'Login success, token =' + res.data.data.token, //defautl axios harus masuk ke data dulu baru ke respons server
+                type: 'success'
+            });
         } catch (err) {
-            console.log(err.response.data);
+            // console.log(err.response.data.msg);
+            setAlert({
+                status: true,
+                message: err?.response?.data?.msg ?? 'Internal server error', //kalau menggunakan axios harus masuk ke response dulu baru ke data
+                type: 'danger'
+            });
         }
         
     }
 
     return (
-        <Container md={12}>
-            <SAlert type="danger" message="test" />
+        <Container md={12} className="my-5">
+            <div className="m-auto" style={{ width : '50%' }}>
+                { alert.status && <SAlert type={alert.type} message={alert.message} />}
+            </div>           
             
             <Card style={{ width: '50%' }} className="m-auto mt-5">
             
